f85e32852cd3265bd515853c2cd71a42af4cce22
Acizza <jonathanmce@gmail.com>
2017-02-09 18:12:58 -0800

    Return error when a file is empty

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,7 @@
 
 use std::path::{Path, PathBuf};
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, Error, ErrorKind};
 
 pub fn local_path(path: &str) -> io::Result<PathBuf> {
     let mut base = ::std::env::current_exe()?;
@@ -34,7 +34,15 @@ pub fn read_file(path: &Path) -> io::Result<String> {
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
 
-    Ok(buffer)
+    if buffer.len() > 0 {
+        Ok(buffer)
+    } else {
+        let path = path.to_str().ok_or(Error::new(
+                        ErrorKind::InvalidData,
+                        "util::read_file(): malformed path"))?;
+        
+        Err(Error::new(ErrorKind::InvalidData, format!("util::read_file(): {} is empty", path)))
+    }
 }
 
 pub fn lerp(v0: f32, v1: f32, t: f32) -> f32 {
