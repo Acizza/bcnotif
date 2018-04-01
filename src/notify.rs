5c784a0e2c48b3f7aed3d5e46a25e9ba12e5a43c
Acizza <killertaco93@gmail.com>
2018-04-01 04:20:55 -0700

    Update winrt to v0.5 Add NullElement variant to NotifyError enum

--- src/notify.rs
+++ src/notify.rs
@@ -11,6 +11,10 @@ pub enum NotifyError {
     #[cfg(windows)]
     #[fail(display = "{:?}", _0)]
     WinRT(::winrt::Error),
+
+    #[cfg(windows)]
+    #[fail(display = "notification element is null: {}", _0)]
+    NullElement(String),
 }
 
 pub enum Icon {
@@ -53,45 +57,52 @@ mod windows {
     use winrt::windows::ui::notifications::*;
     use super::{Icon, NotifyError};
 
+    impl From<::winrt::Error> for NotifyError {
+        fn from(err: ::winrt::Error) -> NotifyError {
+            NotifyError::WinRT(err)
+        }
+    }
+
     // https://stackoverflow.com/a/46817674
     //
     // The Toast Notification Manager needs a valid app ID for any notifications to actually display,
     // so we'll use one that is already defined since it is not worth the effort to create one ourselves.
-    const APP_ID: &str = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe";
-
-    // Having this inner function will allow us to easily map all returned errors into the appropriate
-    // "wrapper" error type with the create() function
-    fn inner_create(title: &str, body: &str) -> Result<(), ::winrt::Error> {
-        unsafe {
-            let toast_xml =
-                ToastNotificationManager::get_template_content(ToastTemplateType::ToastText02)?;
-
-            let toast_text_elements =
-                toast_xml.get_elements_by_tag_name(&FastHString::new("text"))?;
-
-            let add_text = |i, string| {
-                let node = &*toast_xml
-                    .create_text_node(&FastHString::new(string))?
-                    .query_interface::<IXmlNode>()
-                    .unwrap();
-
-                toast_text_elements.item(i)?.append_child(node)
-            };
-
-            add_text(0, title)?;
-            add_text(1, body)?;
-
-            let toast = ToastNotification::create_toast_notification(&*toast_xml)?;
-
-            ToastNotificationManager::create_toast_notifier_with_id(&FastHString::new(APP_ID))?
-                .show(&*toast)?;
-        }
-
-        Ok(())
-    }
+    const APP_ID: &str =
+        "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe";
 
     pub fn create(_: &Icon, title: &str, body: &str) -> Result<(), NotifyError> {
-        inner_create(title, body).map_err(NotifyError::WinRT)
+        let toast_xml = ToastNotificationManager::get_template_content(
+            ToastTemplateType::ToastText02,
+        )?.ok_or_else(|| NotifyError::NullElement("template content".into()))?;
+
+        let toast_text_elements = toast_xml
+            .get_elements_by_tag_name(&FastHString::new("text"))?
+            .ok_or_else(|| NotifyError::NullElement("text elements".into()))?;
+
+        let add_text = |i, string| {
+            let node = &*toast_xml
+                .create_text_node(&FastHString::new(string))?
+                .ok_or_else(|| NotifyError::NullElement("text node".into()))?
+                .query_interface::<IXmlNode>()
+                .ok_or_else(|| NotifyError::NullElement("query interface".into()))?;
+
+            toast_text_elements
+                .item(i)?
+                .ok_or_else(|| NotifyError::NullElement("text item".into()))?
+                .append_child(node)?
+                .ok_or_else(|| NotifyError::NullElement("child node".into()))
+        };
+
+        add_text(0, title)?;
+        add_text(1, body)?;
+
+        let toast = ToastNotification::create_toast_notification(&*toast_xml)?;
+
+        ToastNotificationManager::create_toast_notifier_with_id(&FastHString::new(APP_ID))?
+            .ok_or_else(|| NotifyError::NullElement("toast notification".into()))?
+            .show(&*toast)?;
+
+        Ok(())
     }
 }
 
